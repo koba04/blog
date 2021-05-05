978e0f26af766d9788ad3da16fe264fdd9d6b6f6
Toru Kobayashi <koba0004@gmail.com>
2021-05-06 01:00:08 +0900

    fix: missing dependencies

--- next-env.d.ts
+++ next-env.d.ts
@@ -6,3 +6,11 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module "remark-prism" {
+  export default any;
+}
+
+declare module "remark-linkify-regex" {
+  export default any;
+}
