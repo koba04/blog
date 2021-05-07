b8140d6c23f64c375bd4d33d625fe34a7dfef9b5
Toru Kobayashi <koba0004@gmail.com>
2021-05-07 19:06:08 +0900

    chore: setup to run scripts

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: 'empty'
-        }
-      }
-  
-      return config
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
     }
-  }
+
+    return config;
+  },
+};
---
+++ scripts/generate-rss.ts
@@ -0,0 +1,5 @@
+import { getSortedPostsData } from "../lib/markdown";
+// import { sum } from "./sum";
+
+const posts = getSortedPostsData();
+console.log(posts);
