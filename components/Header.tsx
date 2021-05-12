4f2a4b092756a79b652fb80a5d040914e067ff59
Toru Kobayashi <koba0004@gmail.com>
2021-05-12 23:32:34 +0900

    fix: tweak styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,24 @@
 import styles from "./Header.module.css";
 import Link from "next/link";
+import { TwitterIcon } from "./TwitterIcon";
+import { GitHubIcon } from "./GitHubIcon";
 
 export const Header = () => (
   <header className={`${styles.header} p-4 text-2xl`}>
-    <p className="container p-4">
-      <Link href="/">blog.koba04.com</Link>
+    <p className="container mx-auto max-w-5xl p-0 sm:p-4 flex">
+      <span className="flex-1">
+        <Link href="/">blog.koba04.com</Link>
+      </span>
+      <span className="px-2">
+        <a href="https://twitter.com/koba04" target="_blank" rel="noreferrer">
+          <TwitterIcon />
+        </a>
+      </span>
+      <span className="px-2">
+        <a href="https://github.com/koba04" target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
+      </span>
     </p>
   </header>
 );
--- pages/post/[year]/[month]/[day]/[title]/index.tsx
+++ pages/post/[year]/[month]/[day]/[title]/index.tsx
@@ -1,5 +1,8 @@
 import path from "path";
 import fs from "fs";
+import dayjs from "dayjs";
+
+import styles from "./index.module.css";
 
 import { Header } from "../../../../../../components/Header";
 import { getPostData } from "../../../../../../lib/markdown";
@@ -40,7 +43,17 @@ export default function Post({ postData }: any) {
       <Header />
       <article className="container mx-auto max-w-5xl p-4">
         <h1 className="text-3xl py-4">{postData.title}</h1>
-        <p>{postData.date}</p>
+        <p>
+          {dayjs(postData.date).format("YYYY/MM/DD")}&nbsp;
+          <a
+            href="https://github.com/koba04/"
+            target="_blank"
+            rel="noreferrer"
+            className={styles.author}
+          >
+            @koba04
+          </a>
+        </p>
         <div
           className="content py-8"
           dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
