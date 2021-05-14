54b68cdcc45a84c9a5a2e7cd342a938f87f80391
Toru Kobayashi <koba0004@gmail.com>
2021-05-14 23:44:12 +0900

    feat: add og:image

--- components/Head.tsx
+++ components/Head.tsx
@@ -13,10 +13,7 @@ export const Head = ({ title, url }: { title: string; url: string }) => (
     <meta property="og:title" content={title} />
     <meta property="og:description" content="" />
     <meta property="og:url" content={url} />
-    <meta
-      property="og:image"
-      content="https://avatars.githubusercontent.com/u/250407"
-    />
+    <meta property="og:image" content="/images/ogp.png" />
     <meta property="og:author" content="koba04" />
     <meta property="og:site_name" content="blog.koba04.com" />
     <meta property="og:locale" content="ja_JP" />
