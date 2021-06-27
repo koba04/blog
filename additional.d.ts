b64c13e10a3990eb96530f7e5ad5c61d520fdec6
Toru Kobayashi <koba0004@gmail.com>
2021-06-27 23:04:45 +0900

    chore: add additonal.d.ts instead of editing next-env.d.ts

---
+++ additional.d.ts
@@ -0,0 +1,9 @@
+// https://nextjs.org/docs/basic-features/typescript#existing-projects
+
+declare module "remark-prism" {
+  export default any;
+}
+
+declare module "remark-linkify-regex" {
+  export default any;
+}
