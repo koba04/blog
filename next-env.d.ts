d3fcdc3a49d69081168e7e5634c7b44b0ac2d1ae
Toru Kobayashi <koba0004@gmail.com>
2021-06-27 22:51:23 +0900

    chore: update Next.js to v11

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,16 +1,3 @@
-// / <reference types="next" />
-// / <reference types="next/types/global" />
-// / <reference types="@types/node" />
-
-declare module "*.module.css" {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module "remark-prism" {
-  export default any;
-}
-
-declare module "remark-linkify-regex" {
-  export default any;
-}
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
